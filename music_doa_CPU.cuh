990d9e69e0546937d6a267aab2f2f45537be48c7
Nate <nhatvan1561@gmail.com>
2024-04-16 00:25:41 -0500

    incomplete

--- music_doa_CPU.cuh
+++ music_doa_CPU.cuh
@@ -1,7 +1,15 @@
-#include <stdio.h>
+#define _USE_MATH_DEFINES
 
-double *matrix_mul(double *A, double *B, dim3 dimsA, dim3 dimsB, size_t size){
-	double *C = (double *)malloc(size);
+#include <stdio.h>
+#include <cmath>
+#include <complex>
+
+using namespace std;
+
+const complex double im(0.0,1.0);
+
+complex double *matrix_mul(complex double *A, complex double *B, dim3 dimsA, dim3 dimsB, size_t size){
+	complex double *C = (complex double *)malloc(size);
 	if(dimsA.x != dimsB.y){
 		printf("Invalid matrix multiplication\n");
 		return -1;
@@ -16,27 +24,43 @@ double *matrix_mul(double *A, double *B, dim3 dimsA, dim3 dimsB, size_t size){
 	return C;
 
 }
-double *array_geometry(){
+double *array_geometry(int n_array, double d){
 	//TODO
-	FILE* f_array = fopen(geometry_path, "r");
-
+	//FILE* f_array = fopen(geometry_path, "r");
+	double *ar_geometry = (double *)malloc(n_array);
+	for(int i=0; i<n_array: ++i){
+		ar_geometry[i] = i*d;
+	}
+	return ar_geometry;
 };
 
-double *matrix_transpose(double *A, int N, int M, size_t size){
-	double *C = (double *)malloc(size);
+complex double *matrix_transpose(complex double *A, int N, int M, size_t size){
+	complex double *C = (complex double *)malloc(size);
 	for(int i=0; i<N; ++i){
 		for(int j=0; j<M; ++j){
-			C[i*N+j] = A[j*M+i];
+			C[i*N+j] = conj(A[j*M+i]);
 		}
 	}
 	return C;
 }
 
 
-double *steering_vector(int n_array, int n_sample, double *array_geometry){
+complex double *steering_vector(int n_array, int n_sample, double *array_geometry){
 	//TODO
+	complex double *a = (complex double *)malloc(n_array*n_sample);
+	const double theta = 2*M_PI/n_sample;
+	for(int i=0; i<n_sample; ++i){ //n_sample by n_array matrix
+		for(int j=0; j<n_array; ++j){
+			a[i*n_array+j] = exp(-2 * im* M_PI * array_geometry[j] * sin(theta*i));
+		}
+	}
+	return a;
 }
-double *eigen_wv();
+complex double *cov_matrix(complex double *src_signal, int n_array, int n_sample){
+	//complex double
+}
+
+complex double *eigen_wv();
 double *music();
 double *source_generator(int sample_rate, int n_source);
 
