bd7c646d4ae5cd9ec9165e1f2ac5033181c3ea63
Nate <nhatvan1561@gmail.com>
2024-04-24 23:18:33 -0500

    update

--- test/linear_alg.cpp
+++ test/linear_alg.cpp
@@ -13,7 +13,8 @@ std::complex<double>* conj_transpose(std::complex<double> *src, int M, int N);
 std::complex<double>* cov_matrix(std::complex<double> *src, int M, int N);
 
 int eigen_power(std::complex<double> *src, int M, int N, double ep, std::complex<double>* &ei_value, std::complex<double>* &ei_vector);
-int power_iter(std::complex<double>* &v, int size, double ep);
+int eigen_vector(std::complex<double>* A, std::complex<double>* &v, int size, double ep);
+int eigen_value(std::complex<double>* A, std::complex<double>* v, int size, std::complex<double> &ei_value);
 
 double vec_norm(std::complex<double> *src, int size);
 double random_num(double min=1.0, double max=10.0);
@@ -21,9 +22,9 @@ double random_num(double min=1.0, double max=10.0);
 
 int main(){
 
-	int M = 5;
-	int N = 3;
-	int L = 1;
+	int M = 4;
+	int N = 4;
+	int L = 4;
 
 	std::complex<double> *a;
 //	std::complex<double> *b(1,2);
@@ -45,15 +46,15 @@ int main(){
 	c = matrix_mul(a, b, M, N, N, L);
 	for(int i = 0; i < M; ++i) {
 		for(int j = 0; j < L; ++j) {
-			cout << c[i*M +j] << " ";
+			cout << c[i*L +j] << " ";
 		}
 		cout << endl;
 	}
 
 	printf("Matrix tranpose\n");
 	std::complex<double> *cT = conj_transpose(c, M, L);
-	for(int i = 0; i < M; ++i) {
-		for(int j = 0; j < L; ++j) {
+	for(int i = 0; i < L; ++i) {
+		for(int j = 0; j < M; ++j) {
 			cout << cT[i*M +j] << " ";
 		}
 		cout << endl;
@@ -63,7 +64,7 @@ int main(){
 	std::complex<double> *c_matrix = cov_matrix(c, M, L);
 	for(int i = 0; i < M; ++i) {
 		for(int j = 0; j < L; ++j) {
-			cout << c_matrix[i*M +j] << " ";
+			cout << c_matrix[i*L +j] << " ";
 		}
 		cout << endl;
 	}
@@ -71,12 +72,16 @@ int main(){
 
 	std::complex<double>* ei_value;
 	std::complex<double>* ei_vector;
-	eigen_power(c_matrix, M, M, 0.0001, ei_value, ei_vector);
+	eigen_power(c_matrix, M, M, 0.0001, ei_vector, ei_value);
+	printf("Power Eigen vector\n");
 	for(int i = 0; i < M; ++i) {
-		cout << ei_value[i] << endl;
+		cout << ei_vector[i] << endl;
 	}
+	printf("Power Eigen value\n");
+	cout << ei_value[0] << endl;
 
-	free(ei_value);
+	free(ei_vector);
+	//free(ei_value);
 	free(c_matrix);
 	free(a);
 	free(b);
@@ -107,7 +112,7 @@ std::complex<double>* conj_transpose(std::complex<double> *src, int M, int N){
 	std::complex<double> *result = (std::complex<double> *)malloc(sizeof(std::complex<double>) * N * M);
 	for(int i=0; i<N; ++i){
 		for(int j=0; j<M; ++j){
-			result[i*N+j] = conj(src[j*M+i]);
+			result[i*M+j] = conj(src[j*N+i]);
 		}
 	}
 	return result;
@@ -134,28 +139,42 @@ int eigen_power(std::complex<double> *src, int M, int N, double ep, std::complex
 	for(int i=0; i<N; ++i){
 		v[i] = std::complex<double>(random_num(), random_num());
 	}
-	power_iter(v, N, ep);
+	eigen_vector(src, v, N, ep);
+	eigen_value(src, v, N, ei_vector[0]);
+
 	ei_value = v;
 	return 0;
 }
 
-int power_iter(std::complex<double>* &v, int size, double ep){
+int eigen_vector(std::complex<double>* A, std::complex<double>* &v, int size, double ep){
 	// copy the vector 
 	std::complex<double> *v_old = (std::complex<double> *)malloc(sizeof(std::complex<double>) * size);
 	for(int i=0; i<size; ++i) v_old[i] = v[i];
 
-	double v_norm = vec_norm(v, size);
+	std::complex<double>* Av = matrix_mul(A, v, size, size, size, 1);
+	double Av_norm = vec_norm(Av, size);
 
 	// estimate eigenvector
-	for(int i=0; i<size; ++i) v[i] /= v_norm;
+	for(int i=0; i<size; ++i) Av[i] /= Av_norm;
 
 	// recursive until certain threshold
 	for(int i=0; i<size; ++i){
-		if(abs(v_old[i] - v[i]) > ep){
-			power_iter(v, size, ep);
+		if(abs(v_old[i] - Av[i]) > ep){
+			eigen_vector(A, Av, size, ep);
 			break;
 		}
 	}
+	v = Av;
+	free(v_old);
+	return 0;
+}
+
+int eigen_value(std::complex<double>* A, std::complex<double>* v, int size, std::complex<double> &ei_value){
+	std::complex<double> e_val=0;
+	for(int i=0; i<size; ++i){
+		e_val += A[i] * v[i];
+	}
+	ei_value = e_val/v[0];
 	return 0;
 }
 
