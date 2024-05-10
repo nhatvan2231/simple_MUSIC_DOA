49e4c129dd83d21dfd646edf02a763fc5130fb39
nhatthegreat777 <nhatvan1561@gmail.com>
2024-05-10 02:01:05 -0500

    update

--- test/main.cpp
+++ test/main.cpp
@@ -1,13 +1,14 @@
-//#include "simple_la.h"
+#include "simple_la.h"
 #include "qr_method.h"
+#include <iomanip>
 
 using namespace std;
 
 int main(){
 
-	int M = 4;
+	int M = 3;
 	int N = 4;
-	int L = 2;
+	int L = 3;
 
 	std::complex<double> *a;
 //	std::complex<double> *b(1,2);
@@ -15,18 +16,34 @@ int main(){
 	std::complex<double> *c;
 	a = (std::complex<double> *)malloc(sizeof(std::complex<double>)*M*N);
 	b = (std::complex<double> *)malloc(sizeof(std::complex<double>)*N*L);
+	c = (std::complex<double> *)malloc(sizeof(std::complex<double>)*M*L);
 	//b = (std::complex<double> *)malloc(sizeof(std::complex<double>)*4);
 	for(int i=0; i<M*N; ++i){
 		a[i]= std::complex<double>(random_num(1,2),random_num(1,2));
+		//a[i] = std::complex<double>(i%N, i%N);
+	}
+	printf("Matrix a\n");
+	for(int i = 0; i < M; ++i) {
+		for(int j = 0; j < N; ++j) {
+			cout << a[i*N +j] << " ";
+		}
+		cout << endl;
 	}
 
-	for(int i=0; i<L*N; ++i){
+	for(int i=0; i<N*L; ++i){
 		b[i]= std::complex<double>(random_num(1,2),random_num(1,2));
+		//b[i] = std::complex<double>(i%L, i%L);
 	}
-
+	printf("Matrix b\n");
+	for(int i = 0; i < N; ++i) {
+		for(int j = 0; j < L; ++j) {
+			cout << b[i*L +j] << " ";
+		}
+		cout << endl;
+	}
 
 	printf("Matrix Multiplication\n");
-	c = matrix_mul(a, b, M, N, N, L);
+	matrix_mul(a, b, M, N, N, L, c);
 	for(int i = 0; i < M; ++i) {
 		for(int j = 0; j < L; ++j) {
 			cout << c[i*L +j] << " ";
@@ -34,17 +51,26 @@ int main(){
 		cout << endl;
 	}
 
-	printf("Matrix tranpose\n");
-	std::complex<double> *cT = conj_transpose(c, M, L);
-	for(int i = 0; i < L; ++i) {
+
+	printf("Matrix cov\n");
+	std::complex<double>* c_matrix = (std::complex<double> *)malloc(sizeof(std::complex<double>)*M*M);
+	cov_matrix(c, M, L, c_matrix);
+	for(int i = 0; i < M; ++i) {
 		for(int j = 0; j < M; ++j) {
-			cout << cT[i*M +j] << " ";
+			cout << c_matrix[i*M +j] << " ";
 		}
 		cout << endl;
 	}
-
+	//c_matrix[0] = std::complex<double>(0.5,0);
+	//c_matrix[1] = std::complex<double>(0.75,0);
+	//c_matrix[2] = std::complex<double>(0.5,0);
+	//c_matrix[3] = std::complex<double>(1.0,0);
+	//c_matrix[4] = std::complex<double>(0.5,0);
+	//c_matrix[5] = std::complex<double>(0.75,0);
+	//c_matrix[6] = std::complex<double>(0.25,0);
+	//c_matrix[7] = std::complex<double>(0.25,0);
+	//c_matrix[8] = std::complex<double>(0.25,0);
 	printf("Matrix cov\n");
-	std::complex<double> *c_matrix = cov_matrix(c, M, L);
 	for(int i = 0; i < M; ++i) {
 		for(int j = 0; j < M; ++j) {
 			cout << c_matrix[i*M +j] << " ";
@@ -52,29 +78,20 @@ int main(){
 		cout << endl;
 	}
 
-
-	std::complex<double>* R = (std::complex<double>*)malloc(sizeof(std::complex<double>)*M*M);
-	householder_reflections(c_matrix, M, R);
-	printf("R matrix\n");
+	std::complex<double>* r_test = (std::complex<double> *)calloc(M*M,sizeof(std::complex<double>));
+	householder_reflections(c_matrix, M, r_test);
+	printf("R\n");
 	for(int i = 0; i < M; ++i) {
 		for(int j = 0; j < M; ++j) {
-			cout << R[i*M +j] << " ";
+			cout << setw(20) << r_test[i*M +j] << " ";
 		}
 		cout << endl;
 	}
-	//std::complex<double>* ei_value;
-	//std::complex<double>* ei_vector;
-	//eigen_power(c_matrix, M, M, 0.0001, ei_vector, ei_value);
-	//printf("Power Eigen vector\n");
-	//for(int i = 0; i < M; ++i) {
-	//	cout << ei_vector[i] << endl;
-	//}
-	//printf("Power Eigen value\n");
-	//cout << ei_value[0] << endl;
+	free(r_test);
 
-	//free(ei_vector);
-	//free(ei_value);
-	free(R);
+
+
+
 	free(c_matrix);
 	free(a);
 	free(b);
--- test/qr_method.cpp
+++ test/qr_method.cpp
@@ -1,6 +1,6 @@
 #include "qr_method.h"
+#include <iomanip>
 
-using namespace std;
 
 /********************************************
  ***********QR decomposition method**********
@@ -10,17 +10,22 @@ using namespace std;
 
 int householder_reflections(std::complex<double>* A, int size, std::complex<double>* &R){
 	//std::complex<double>* R = (std::complex<double> *)malloc(sizeof(std::complex<double>) * size * size);
-	std::complex<double>* tmp_A = matrix_copy(A, size, size);
+	//std::complex<double>* tmp_A = (std::complex<double> *)malloc(sizeof(std::complex<double>) * size * size); 
+	//matrix_copy(A, size, size, tmp_A);
+	matrix_copy(A, size, size, R);
 	for(int i=0; i<size-1; ++i){
-		std::complex<double>* ai = get_col(A, size, size, i);
+		std::complex<double>* tmp_A = (std::complex<double> *)calloc(size*size, sizeof(std::complex<double>));
+		std::complex<double>* subA = (std::complex<double> *)malloc(sizeof(std::complex<double>) * size);
 		std::complex<double>* P = (std::complex<double> *)malloc(sizeof(std::complex<double>) * size * size);
-		compute_P(ai, size, i, P);
-		R = matrix_mul(P, tmp_A, size, size, size, size);
-		tmp_A = matrix_copy(R, size, size);
-		free(ai);
+		get_col(R, size, size, i, subA);
+		identity_mat(size, P); // match the original size of A
+		compute_P(subA, size, i, P);
+		matrix_mul(P, R, size, size, size, size, tmp_A);
+		matrix_copy(tmp_A, size, size, R);
 		free(P);
+		free(subA);
+		free(tmp_A);
 	}
-	free(tmp_A);
 	return 0;
 }
 
@@ -28,30 +33,55 @@ int compute_P(std::complex<double>* colA, int size, int col, std::complex<double
 	// P = I - 2 * n * nT
 	// I is identity matrix
 	int size_i = size - col;
-	mat_P = (std::complex<double>*)identity_mat(size);
-	double* mat_i = identity_mat(size_i);
-	std::complex<double>* ai = colA + col; // column offset in pointer
+	std::complex<double>* mat_i = (std::complex<double> *)malloc(sizeof(std::complex<double>) * size_i * size_i);
 	std::complex<double>* vec_n = (std::complex<double> *)malloc(sizeof(std::complex<double>) * size_i);
+	std::complex<double>* vec_ncov = (std::complex<double> *)malloc(sizeof(std::complex<double>) * size_i * size_i);
+
+	identity_mat(size_i, mat_i); // size of sub A
+	std::complex<double>* ai = colA + col; // column offset in pointer
+	//printf("col A\n");
+	//for(int i=0; i<size_i; ++i){
+	//	std::cout << ai[i] << std::endl;
+	//}
 	compute_n(ai, size_i, vec_n);
+	cov_matrix(vec_n, size_i ,1 , vec_ncov);
 	for(int i=0; i<size_i; ++i){
 		for(int j=0; j<size_i; ++j){
 			int ii = i+col;
 			int jj = j+col;
-			mat_P[ii*size+jj] = mat_i[i*size_i+j] - (2.0 * vec_n[i*size_i+j] * conj(vec_n[j*size_i+i]));
+			//mat_P[ii*size+jj] = mat_i[i*size_i+j] - (2.0 * vec_n[i*size_i+j] * conj(vec_n[j*size_i+i]));
+			mat_P[ii*size+jj] = mat_i[i*size_i+j] - (2.0 * vec_ncov[i*size_i+j]);
 		}
+		//std::cout << std::endl;
 	}
+	//printf("Compute P %d\n", col);
+	//for(int i = 0; i < size; ++i) {
+	//	for(int j = 0; j < size; ++j) {
+	//		std::cout << std::setw(25) << std::setprecision(5) << mat_P[i*size +j] << " ";
+	//	}
+	//	std::cout << std::endl;
+	//}
+	free(mat_i);
+	free(vec_ncov);
+	//free(ai);
+	free(vec_n);
+	//printf("DONE P\n");
 	return 0;
 }
 
 int compute_n(std::complex<double>* colA, int size, std::complex<double>* &vec_n){
-	// vector n = vector n/ norm(n)
+	// vector n = vector u/ norm(u)
 	std::complex<double>* vec_u = (std::complex<double> *)malloc(sizeof(std::complex<double>) * size);
-	double u_norm = vec_norm(vec_u, size); // get the norm of vector u
 	compute_u(colA, size, vec_u); // get u vector
+	double u_norm = vec_norm(vec_u, size); // get the norm of vector u
 	
+	//printf("Compute n\n");
 	for(int i=0; i<size; ++i){
 		vec_n[i] = vec_u[i]/u_norm;
+		//std::cout << vec_n[i] << std::endl;
 	}
+	free(vec_u);
+	//printf("DONE n\n");
 	return 0;
 
 }
@@ -59,16 +89,21 @@ int compute_u(std::complex<double>* colA, int size, std::complex<double>* &vec_u
 	// vector u = vector Ai - (sign * norm(Ai) * vector b)
 	// vector b is the vector for reflection -> b = [1, 0, 0]
 	double A_norm = vec_norm(colA, size);
-	int sign = -1 * real(colA[0]) / real(colA[0]); // opposite sign to first element of column 
+	std::complex<double> sign;
+	if (real(colA[0]) > 0) sign = std::complex<double>(-1,0);
+	else sign = std::complex<double>(1,0); // opposite sign to first element of column 
 	
 	// vector for reflection
-	double vec_b[size] = {};
+	double* vec_b = (double*)malloc(sizeof(double)*size);
+	//double vec_b[size] = {};
 	vec_b[0] = 1;
 	
-	printf("vector u\n");
 	for(int i=0; i<size; ++i){
-		vec_u[i] = colA[i] - (sign * A_norm * vec_b[i]);
-		cout << vec_u[i] << endl;
+		std::complex<double> tmp = sign *   A_norm * vec_b[i];
+		vec_u[i] = colA[i] - tmp;
+		//std::cout << vec_u[i] << " " << colA[i] <<  std::endl;
 	}
+	//printf("DONE u\n");
+	free(vec_b);
 	return 0;
 }
